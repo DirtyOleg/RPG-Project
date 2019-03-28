13ded58b8dd0a796324c4388db24b416f27017a3
DESKTOP-J8S11UC\Hive Dev <>
2019-03-28 15:20:01 -0400

    Initial commit

---
+++ Assets/Script/Core/CameraMovementHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementHandler : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform cameraOrigin;
+
+    private void LateUpdate() 
+    {
+        cameraOrigin.position = player.position;
+    }
+}
