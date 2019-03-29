fcc17ede92eabc7b8a2f13cd89593cf12f54f072
DESKTOP-J8S11UC\Hive Dev <>
2019-03-29 12:36:58 -0400

    First Draft of player movement controller

---
+++ Assets/Script/Character Controller/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.CharactorController
+{
+    [RequireComponent(typeof(MovementController))]
+    public class PlayerController : MonoBehaviour
+    {
+        private MovementController movementController;
+        private NavMeshAgent navMeshAgent;
+        private Animator anim;
+
+        private void Start()
+        {
+            movementController = GetComponent<MovementController>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            anim = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                MouseClick();
+            }
+
+            AnimationHandler();
+        }
+
+        private void MouseClick()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, 100f))
+            {
+                movementController.MoveToPosition(hitInfo.point);
+            }
+        }
+
+        private void AnimationHandler()
+        {
+            Vector3 playerVelocity = navMeshAgent.velocity;
+            float forwardSpeed = playerVelocity.z;
+
+            anim.SetFloat("forwardSpeed", forwardSpeed);
+        }
+    }
+}
--- Assets/Script/Core/MovementController.cs
+++ Assets/Script/Core/MovementController.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class MovementController : MonoBehaviour
+namespace RPG.Core
 {
-    
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class MovementController : MonoBehaviour
+    {
+        private NavMeshAgent navMeshAgent;
+
+        private void Start() 
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+
+        public void MoveToPosition(Vector3 targetPosition)
+        {
+            navMeshAgent.destination = targetPosition;
+        }
+    }
 }
